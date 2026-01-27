fa28cc2660f654368afa8ac5499be0f735a40920
Mapi <matvey2000000009@gmail.com>
2026-01-27 14:34:15 +0100

    updated cloudbuild

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 # Retrieve individual components from Environment Variables
 db_user = os.getenv("DB_USER", "postgres")
-db_pass = os.getenv("DB_PASSWORD", "password") # This comes from your Secret
+db_pass = os.getenv("DB_PASSWORD", "supersecurepassword") # This comes from your Secret
 db_host = os.getenv("INSTANCE_CONNECTION_NAME", "localhost") 
 NOWPAYMENTS_API_KEY = os.getenv("NOWPAYMENTS_API_KEY")
 NOWPAYMENTS_IPN_SECRET = os.getenv("NOWPAYMENTS_IPN_SECRET")
@@ -17,7 +17,7 @@ crypto_db_name = os.getenv("DB_NAME_CRYPTO", "crypto_backend_db")
 # Function to build URL
 def get_db_url(db_name):
     # Retrieve and URL-encode the password
-    raw_pass = os.getenv("DB_PASSWORD", "password")
+    raw_pass = os.getenv("DB_PASSWORD", "supersecurepassword")
     encoded_pass = urllib.parse.quote_plus(raw_pass)  # <--- USE THIS
 
     is_ip = "." in db_host and ":" not in db_host
